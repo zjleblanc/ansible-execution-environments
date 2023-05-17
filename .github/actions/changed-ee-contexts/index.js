286015f7c5c5732c41fddf8bf68bd2f703589fcf
zjleblanc <zjleblanc3@gmail.com>
2023-05-17 12:04:17 -0500

    add flag for ee-build-required

--- .github/actions/changed-ee-contexts/index.js
+++ .github/actions/changed-ee-contexts/index.js
@@ -6,16 +6,18 @@ try {
   console.log(changed_files);
   if (changed_files === undefined || changed_files === '') {
     core.setOutput("changed-ees", []);
+    core.setOutput("build-required", false);
     return;
   } 
 
   const changed_ees = changed_files
     .split(" ")
     .map(f => f.split("/")[1]);
-    
+
   const deduped_ees = [...new Set(changed_ees)];
   console.log(deduped_ees);
   core.setOutput("changed-ees", deduped_ees);
+  core.setOutput("build-required", deduped_ees.length > 0);
 
 } catch (error) {
   core.setFailed(error.message);
