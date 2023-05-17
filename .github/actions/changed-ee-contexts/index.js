02be28742fa7615f9d789eece077effe64e0a200
zjleblanc <zjleblanc3@gmail.com>
2023-05-17 11:59:10 -0500

    add empty handling in action

--- .github/actions/changed-ee-contexts/index.js
+++ .github/actions/changed-ee-contexts/index.js
@@ -1,10 +1,18 @@
 const core = require('@actions/core');
 
 try {
-  const changed_files = core.getInput('changed-files').split(" ");
-  console.log(changed_files);
+  const changed_files = core.getInput('changed-files');
 
-  const changed_ees = changed_files.map(f => f.split("/")[1]);
+  console.log(changed_files);
+  if (changed_files === undefined || changed_files === '') {
+    core.setOutput("changed-ees", []);
+    return;
+  } 
+
+  const changed_ees = changed_files
+    .split(" ")
+    .map(f => f.split("/")[1]);
+    
   const deduped_ees = [...new Set(changed_ees)];
   console.log(deduped_ees);
   core.setOutput("changed-ees", deduped_ees);
